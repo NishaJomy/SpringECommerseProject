bca3cf1374d0f06385a988733537a7f501de4d6b
Nisha George <nishajomy.2007@gmail.com>
2023-09-12 19:52:25 -0500

    created a spring boot project

---
+++ ECommerse/src/main/java/org/launchcode/ECommerse/ECommerseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.ECommerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerseApplication.class, args);
+	}
+
+}
