57232537ceb97c3c2e4b9c23128c78967c9caabb
Nisha George <nishajomy.2007@gmail.com>
2023-09-20 19:19:15 -0500

    added model classes

---
+++ ECommerse/src/main/java/org/launchcode/ECommerse/models/Cart.java
@@ -0,0 +1,55 @@
+package org.launchcode.ECommerse.models;
+
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private User customer;
+
+    @ManyToMany
+    @JoinTable(
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+}
---
+++ ECommerse/src/main/java/org/launchcode/ECommerse/models/Category.java
@@ -0,0 +1,4 @@
+package org.launchcode.ECommerse.models;
+
+public class Category {
+}
